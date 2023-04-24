67f899b5eb0c51f36edb8c9c0d9924f6fc495e28
Andras Gyarmati <bandieee16@gmail.com>
2023-04-24 19:18:54 +0200

    mock login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UntypedFormBuilder, UntypedFormGroup, Validators} from '@angular/forms';
+import {ClientService} from "../_services/client.service";
+import {RouterService} from "../_services/router.service";
+import {UserService} from "../_services/user.service";
 
 @Component({
   selector: 'app-login',
@@ -12,6 +15,10 @@ export class LoginComponent implements OnInit {
   submitForm(): void {
     if (this.loginForm.valid) {
       console.log('submit', this.loginForm.value);
+      this.clientService.login().subscribe((response) => {
+        this.userService.store(response.token);
+        this.routerService.routeToLobby();
+      })
     } else {
       Object.values(this.loginForm.controls).forEach(control => {
         if (control.invalid) {
@@ -22,7 +29,10 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  constructor(private fb: UntypedFormBuilder) {
+  constructor(private fb: UntypedFormBuilder,
+              private clientService: ClientService,
+              private routerService: RouterService,
+              private userService: UserService) {
   }
 
   ngOnInit(): void {
